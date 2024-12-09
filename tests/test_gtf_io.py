fc4d02a79dab9d7ff4b30545a2e49dced44e99a3
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-12-09 14:17:56 +0100

    Update the `three` and `four` tests.

--- tests/test_gtf_io.py
+++ tests/test_gtf_io.py
@@ -34,10 +34,12 @@ class TestGtfIo:
         assert one.start == 44_668_712
         assert one.end == 44_668_805
         assert one.strand == Strand.POSITIVE
+
         assert two.contig.name == "22"
         assert two.start == 44_702_491
         assert two.end == 44_702_501
         assert two.strand == Strand.POSITIVE
+
 
         # Negative strand
         for tx in transcripts:
@@ -46,24 +48,15 @@ class TestGtfIo:
                 break
 
         assert our_another_favorite_tx is not None
-        assert len(our_another_favorite_tx._five_utr.regions) == 2
+        assert len(our_another_favorite_tx.five_utr.regions) == 2
         three, four = sorted(our_another_favorite_tx.five_utr.regions, key=lambda region: region.start)
 
         assert three.contig.name == "22"
-        assert three.start == 26_837_999
-        assert three.end == 26_838_057
+        assert three.start_on_strand(Strand.POSITIVE) == 23_980_411
+        assert three.end_on_strand(Strand.POSITIVE) == 23_980_469
         assert three.strand == Strand.NEGATIVE
+        
         assert four.contig.name == "22"
-        assert four.start == 26_841_401
-        assert four.end == 26_841_576
+        assert four.start_on_strand(Strand.POSITIVE) == 23_976_892
+        assert four.end_on_strand(Strand.POSITIVE) == 23_977_067
         assert four.strand == Strand.NEGATIVE
-
-        three_in_positive_strand = three.with_strand(other= Strand.POSITIVE)
-        three_one_based_start = three_in_positive_strand.start + 1
-        assert three_one_based_start == 23_980_412
-        assert three_in_positive_strand.end == 23_980_469
-
-        four_in_positive_strand = three.with_strand(other= Strand.POSITIVE)
-        four_one_based_start = four_in_positive_strand.start + 1
-        assert four_one_based_start == 23_980_412
-        assert four_in_positive_strand.end == 23_980_469
