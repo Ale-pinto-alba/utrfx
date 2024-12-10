5d25bd00f8cc101025dd35f7979415432411f4d6
Alejandro Pinto Alba <alejandropintoalba@gmail.com>
2024-12-10 15:41:10 +0100

    uorf and model scripts reshaped

--- src/utrfx/model.py
+++ src/utrfx/model.py
@@ -1,17 +1,23 @@
 import typing
     
-from .genome import GenomicRegion
+from .genome import GenomicRegion, Region
+
 
 class FiveUTR:
     """
     `FiveUTR` is a container for 5'UTR Genomic Regions.
-    """
 
+    :param regions: list of Genomic Regions corresponding to the 5'UTRs regions.
+    """
     def __init__(
         self,
         regions: typing.Collection[GenomicRegion],
     ):
         self._regions = regions
+
+    @property
+    def regions(self) -> typing.Collection[GenomicRegion]:
+        return self._regions
 
     def __len__(self) -> int:
         return 0 if len(self._regions) == 0 else sum(len(region) for region in self._regions)
@@ -19,17 +25,15 @@ class FiveUTR:
     def __repr__(self):
         regions_info = ", ".join([f"({region._contig.ucsc_name}, {region.start}, {region.end}, {region.strand})" for region in self._regions])
         return f"FiveUTR(regions={len(self._regions)} regions: {regions_info})"
-
-    @property
-    def regions(self) -> typing.Collection[GenomicRegion]:
-        return self._regions
     
 
 class Transcript:
     """
     `Transcript` represents the 5'UTR Genomic Region(s) of a transcript.
+
+    :param tx_id: GENCODE transcript identifier, e.g. `ENST00000381418`
+    :param five_utr: 5'UTR Genomic Region(s). 
     """
-    
     def __init__(
         self, 
         tx_id: str, 
@@ -48,4 +52,94 @@ class Transcript:
 
     def __repr__(self):
         return f"Transcript(tx_id={self._tx_id}, five_utr={repr(self._five_utr)})"
+
+
+class FivePrimeSequence:
+    """
+    `FivePrimeSequence` represents the 5'UTR cDNA sequence of a transcript.
+
+    :param transcript: transcript with its GENCODE ID and 5'UTR Genomic Region(s).
+    :param five_prime_sequence: 5'UTR nucleotide sequence.
+    """
+    def __init__(
+        self,
+        transcript: Transcript,
+        five_prime_sequence: str,
+    ):
+        self._transcript = transcript
+        self._five_prime_sequence = five_prime_sequence.upper()
+
+    @property
+    def transcript(self) -> typing.Sequence[GenomicRegion]:
+        return self._transcript
+
+    @property
+    def five_prime_sequence(self) -> str:
+            return self._five_prime_sequence
+        
+    # @staticmethod
+    # def from_fasta(tx: Transcript, fpath: str) -> "FivePrimeSequence":
+    #     valid_fasta_extensions = (".fa", ".fasta", ".fna", ".ffn", ".faa", ".frn")
+
+    #     if fpath.endswith(valid_fasta_extensions):
+    #         with open(fpath) as f:
+    #             lines = f.readlines()
+    #             return FivePrimeSequence(
+    #                 tx= Transcript(tx_id= tx.tx_id, five_utr= tx.five_utr),
+    #                 five_prime_sequence= "".join(line.strip() for line in lines[1:]),
+    #             )
+    #     else:
+    #         raise ValueError("Unexpected extension.")
+
+    def __len__(self) -> int:
+        return len(self._five_prime_sequence)
     
+    def __eq__(self, other):
+        return (isinstance(other, FivePrimeSequence)
+                and self._transcript == other._transcript
+                and self._five_prime_sequence == other._five_prime_sequence)
+
+    def __repr__(self) -> str:
+        return f"FivePrimeSequence(tx_id= {self._transcript.tx_id}, 5'UTRs= {self._transcript.five_utr}, 5'UTR_sequence= {self.five_prime_sequence})"
+    
+
+class UORF:
+    """
+    `UORF` represents an uORF cDNA sequence of a transcript.
+
+    The UORF is upstream of the mORF and they do *not* overlap.
+
+    :param five_prime: transcript with its GENCODE ID, 5'UTR Genomic Region(s) and cDNA sequence.
+    :param uorf: uORF region marked by its start and end nucleotide.
+    """
+    def __init__(
+        self,
+        five_prime: FivePrimeSequence,
+        uorf: Region,
+    ):
+        self._five_prime = five_prime
+        self._uorf = uorf
+
+    @property
+    def uorf_sequence(self) -> str:
+        return self._five_prime.five_prime_sequence[self._uorf.start: self._uorf.end]
+
+    def intercistonic_distance(self) -> int:
+        """
+        Calculate the intercistonic distance, which is the number of bases located between the uORF stop codon 
+        and the mORF start codon, starting at the nucleotide (included) just after the uORF stop codon.
+        """
+        intercistonic_distance = len(self._five_prime.five_prime_sequence) - self._uorf.end
+
+        return intercistonic_distance
+
+    def __len__(self) -> int:
+        return len(self.uorf_sequence)  
+
+    def __eq__(self, other):
+        return (isinstance(other, UORF)
+                and self._five_prime== other._five_prime
+                and self._uorf == other._uorf)
+    
+    def __repr__(self) -> str:
+        return f"UORF(tx_id= {self._five_prime._transcript.tx_id}, 5'UTRs= {self._five_prime._transcript.five_utr}, 5'UTR_sequence= {self._five_prime.five_prime_sequence}, uORF= {self._uorf})"
--- src/utrfx/uorf.py
+++ src/utrfx/uorf.py
@@ -1,117 +1,41 @@
 import abc
 import typing
 
-from utrfx.genome import GenomicRegion
-from utrfx.model import Transcript
-from utrfx.util import GenomicSequenceService
+import requests
 
+from utrfx.genome import Region, GenomeBuild, GenomicRegion, Strand, GRCh38
+from utrfx.model import FiveUTR, FivePrimeSequence, UORF, Transcript
 
-class FivePrimeSequence:
+def download_fasta_from_ensembl(transcript_id: str, output_file: str) -> typing.IO:
     """
-    `FivePrimeSequence` represents the 5'UTR cDNA sequence of a transcript.
+    Download a FASTA file containing the cDNA sequence for a given transcript from Ensembl's REST API.
 
-    :param tx_id: GENCODE transcript identifier e.g. `ENST00000381418_9`
-    :param five_prime_sequence: 5'UTR nucleotide sequence.
+    :param transcript_id: Ensembl transcript identifier e.g. `ENST00000381418`
+    :param output_file: the path to save the downloaded FASTA file.
     """
-    def __init__(
-        self,
-        tx_id: str,
-        utrs: typing.Sequence[GenomicRegion],
-        five_prime_sequence: str,
-    ):
-        self._tx_id = tx_id
-        self._utrs = utrs
-        self._five_prime_sequence = five_prime_sequence.upper()
+    base_url = f"https://rest.ensembl.org/sequence/id/{transcript_id}?content-type=text/x-fasta"
 
-    @property
-    def tx_id(self) -> str:
-        return self._tx_id
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        with open(output_file, 'w') as f:
+            f.write(response.text)
+        print(f"FASTA file successfully downloaded and saved to {output_file}")
+    else:
+        raise Exception(f"Error: Unable to download FASTA. HTTP Status Code: {response.status_code}")
     
-    @property
-    def utrs(self) -> typing.Sequence[GenomicRegion]:
-        return self._utrs
 
-    @property
-    def five_prime_sequence(self) -> str:
-            return self._five_prime_sequence
-        
-    @staticmethod
-    def from_fasta(tx: Transcript, fpath: str) -> "FivePrimeSequence":
-        valid_fasta_extensions = (".fa", ".fasta", ".fna", ".ffn", ".faa", ".frn")
-
-        if fpath.endswith(valid_fasta_extensions):
-            with open(fpath) as f:
-                lines = f.readlines()
-                return FivePrimeSequence(
-                    tx_id=tx.tx_id, 
-                    utrs=tx.five_utr.regions,
-                    five_prime_sequence= "".join(line.strip() for line in lines[1:]),
-                )
-        else:
-            raise ValueError("Unexpected extension.")
-
-    def __len__(self) -> int:
-        return len(self._five_prime_sequence)
-    
-    def __eq__(self, other):
-        return (isinstance(other, FivePrimeSequence)
-                and self.tx_id == other.tx_id
-                # TODO
-                and self.five_prime_sequence == other.five_prime_sequence)
-
-    def __repr__(self) -> str:
-        return f"FivePrimeSequence(tx_id= {self.tx_id}, 5'UTR= {self.five_prime_sequence})" # TODO
-
-
-class UORF:
+def get_five_prime_sequence(transcript_sequence: str, five_utrs: FiveUTR) -> str:
     """
-    `UORF` represents an uORF cDNA sequence of a transcript.
+    Return the 5'UTR cDNA sequence of a given transcript nucleotide sequence.
 
-    The UORF is upstream of the mORF and they do *not* overlap.
-
-    :param tx_id: GENCODE transcript identifier e.g. `ENST00000381418_9`
-    :param uorf: uORF nucleotide sequence.
+    :param transcript_sequence: FASTA file with the transcript nucleotide sequence.
+    :param five_utrs: 5'UTR Genomic Region(s).
     """
-    def __init__(
-        self,
-        five_prime: FivePrimeSequence,
-        uorf: str,
-    ):
-        self._five_prime = five_prime
-        self._uorf = uorf.upper()
-
-    @property
-    def tx_id(self) -> str:
-        return self._five_prime.tx_id
-    
-    @property
-    def five_prime(self) -> FivePrimeSequence:
-        return self._five_prime
-
-    @property
-    def uorf_sequence(self) -> str:
-        return self._uorf
-
-    def intercistonic_distance(self) -> int:
-        """
-        Calculate the intercistonic distance, which is the number of bases located between the uORF stop codon 
-        and the mORF start codon, starting at the nucleotide (included) just after the uORF stop codon.
-        """
-        start_index = self._five_prime.five_prime_sequence.find(self.uorf_sequence) + len(self.uorf_sequence)
-        intercistonic_distance = len(self._five_prime.five_prime_sequence) - start_index
-
-        return intercistonic_distance
-
-    def __len__(self) -> int:
-        return len(self._uorf)  
-
-    def __eq__(self, other):
-        return (isinstance(other, UORF)
-                and self.tx_id == other.tx_id  # TODO
-                and self.uorf_sequence == other.uorf_sequence)
-    
-    def __repr__(self) -> str:
-        return f"UORF(tx_id= {self.tx_id}, uORF= {self.uorf_sequence})"  # TODO
+    with open(transcript_sequence) as f:
+        lines = f.readlines()
+        sequence = "".join(line.strip() for line in lines[1:])
+        return sequence[:five_utrs.__len__()]
 
 
 def uorf_extractor(five_utr: FivePrimeSequence) -> typing.Collection[UORF]:
@@ -120,32 +44,29 @@ def uorf_extractor(five_utr: FivePrimeSequence) -> typing.Collection[UORF]:
     """
     five_sequence = five_utr._five_prime_sequence
     uorfs = []
-        
-    while "ATG" in five_sequence:  
-        codon_list = []  
+    start_position = 0
 
-        start_index = five_sequence.find("ATG")
+    while start_position < len(five_sequence) - 3:
+        start_index = five_sequence.find("ATG", start_position)
         if start_index < 0:
-            # No more UORF in the remaining sequence
-            break
-    
-        found_stop = False 
-            
-        for i in range(start_index, len(five_sequence) - len(five_sequence) % 3, 3):
+            break  # No more uORF in the remaining sequence
+
+        found_stop = False
+        for i in range(start_index, len(five_sequence) - 2, 3):  
             codon = five_sequence[i:i + 3]
-            assert len(codon) == 3, "Codons must be 3nt long."
-
-            codon_list.append(codon)
-
             if codon in ["TAA", "TAG", "TGA"]:
+                stop_index = i + 3
                 found_stop = True
                 break
 
         if found_stop:
-            uorf = UORF(tx_id= five_utr.tx_id, uorf= "".join(codon_list))
-            uorfs.append(uorf)
-
-        five_sequence = five_sequence[start_index + 3:]
+            uorfs.append(UORF(
+                five_prime=five_utr,
+                uorf=Region(start=start_index, end=stop_index)
+            ))
+            start_position = stop_index  
+        else:
+            start_position = start_index + 3  
 
     return uorfs
 
@@ -166,106 +87,44 @@ def gc_content(uorf: UORF) -> float:
 
         return gc_content
     
-
-
 class GcContentCalculator:
 
     def __init__(
         self,
-        genome_sequence_service: GenomicSequenceService,
-    ):
-        assert isinstance(genome_sequence_service, GenomicSequenceService)
-        self._service = genome_sequence_service
-
-
-    def gc_content_of_the_ten_nts_after(
-        self,
-        # five_utr: FivePrimeSequence,
         uorf: UORF,
-    ) -> float:
+    ):
+        self._uorf = uorf
+
+    def gc_content_n_bases_downstream(self, bases: int) -> float:
         """
         """
-        # TODO: create a genomic region that spans `n` bases downstream of the last UORF base.
-        # fetch sequence from the service
-        # compute GC content
+        if bases > (len(self._uorf._five_prime.five_prime_sequence) - self._uorf._uorf.end):
+            bases = len(self._uorf._five_prime.five_prime_sequence) - self._uorf._uorf.end
+            print(f"Limit exceed, calculating the GC content of the {total} nucleotides after the uORF")
 
-        # Side task - clean up redundant branches.
-        uorf.five_prime.utrs
-
-        if uorf.is_in_five_utr(five_utr= five_utr) == False or five_utr.tx_id != uorf.tx_id:
-            raise ValueError("The uORF is not in the given 5'UTR region.")
-        else:
-            start_index = five_utr.five_prime_sequence.find(uorf.uorf_sequence) + len(uorf.uorf_sequence)
-            ten_nt_after = five_utr.five_prime_sequence[start_index: start_index + 10]
-            
-            total = len(ten_nt_after)
-            if total == 0:
-                raise ValueError("Cannot calculate GC content from a 0 nt region.")
-            
-            elif total < 10:
-                print(f"Calculating the GC content of the {total} nucleotides after the uORF")
-
-                g = ten_nt_after.count("G")
-                c = ten_nt_after.count("C")
-                gc_content = ((g+c)/total) * 100
-                return gc_content
-            
-            else:
-
-                g = ten_nt_after.count("G")
-                c = ten_nt_after.count("C")
-                gc_content = ((g+c)/10) * 100
-                return gc_content
-
-
-def gc_content_of_the_ten_nts_after(five_utr: FivePrimeSequence, uorf: UORF) -> float:
-    """
-    """
-    if uorf.is_in_five_utr(five_utr= five_utr) == False or five_utr.tx_id != uorf.tx_id:
-        raise ValueError("The uORF is not in the given 5'UTR region.")
-    else:
-        start_index = five_utr.five_prime_sequence.find(uorf.uorf_sequence) + len(uorf.uorf_sequence)
-        ten_nt_after = five_utr.five_prime_sequence[start_index: start_index + 10]
-        
-        total = len(ten_nt_after)
+        region_downstream = Region(start= self._uorf._uorf.end, end= self._uorf._uorf.end + bases)
+        total = region_downstream.__len__()
         if total == 0:
-            raise ValueError("Cannot calculate GC content from a 0 nt region.")
-        
-        elif total < 10:
-            print(f"Calculating the GC content of the {total} nucleotides after the uORF")
-
-            g = ten_nt_after.count("G")
-            c = ten_nt_after.count("C")
+            return "GC content equals 0"
+        else:
+            g = self._uorf._five_prime.five_prime_sequence[region_downstream.start: region_downstream.end].count("G")
+            c = self._uorf._five_prime.five_prime_sequence[region_downstream.start: region_downstream.end].count("C")
             gc_content = ((g+c)/total) * 100
-            return gc_content
-        
-        else:
 
-            g = ten_nt_after.count("G")
-            c = ten_nt_after.count("C")
-            gc_content = ((g+c)/10) * 100
             return gc_content
 
 
-def uorfs_plus_20nt_extractor(five_utr: FivePrimeSequence, uorf: UORF) -> str:
+def uorfs_plus_n_nts_downstream_extractor(uorf: UORF, bases: int) -> str:
     """ 
-    Get the uORF plus the twenty nucleotides after the uORF stop codon (if possible) for indel analysis.
+    Get the uORF plus the `n` nucleotides after the uORF stop codon (if possible) for indel analysis.
     """
-    if uorf.is_in_five_utr(five_utr= five_utr) == False or five_utr.tx_id != uorf.tx_id:
-        raise ValueError("The uORF is not in the given 5'UTR region.")
-    else:
-        start_index = five_utr.five_prime_sequence.find(uorf.uorf_sequence) + len(uorf.uorf_sequence)
-        twenty_nt_after = five_utr.five_prime_sequence[start_index: start_index + 20]
+    if bases > (len(uorf._five_prime.five_prime_sequence) - uorf._uorf.end):
+            bases = len(uorf._five_prime.five_prime_sequence) - uorf._uorf.end
+            print(f"Limit exceed, taking the uORF plus the {bases} nucleotides downstream")
 
-        if twenty_nt_after != 20:
-            print(f"Taking the {twenty_nt_after} nucleotides after instead of 20.")
-            uorf_plus_nt = uorf.uorf_sequence + twenty_nt_after
-            
-            return uorf_plus_nt
-        else:
-            uorf_plus_20nt = uorf.uorf_sequence + twenty_nt_after
-        
-            return uorf_plus_20nt
+    region_downstream = Region(start= uorf._uorf.end, end= uorf._uorf.end + bases)
+
+    return uorf.uorf_sequence + uorf._five_prime.five_prime_sequence[region_downstream.start: region_downstream.end]
 
 
 class KozakSequenceCalculator(metaclass=abc.ABCMeta):
