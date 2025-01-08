d70e409084d50663dd5cf94a2e21bf661ae2ffa0
Alejandro Pinto Alba <alejandropintoalba@gmail.com>
2025-01-08 12:54:25 +0100

    Correct gc content tests

--- tests/test_uorf.py
+++ tests/test_uorf.py
@@ -8,9 +8,9 @@ from utrfx.uorf import gc_content, gc_content_n_bases_downstream, uorfs_plus_n_n
 @pytest.mark.parametrize(
     "region, expected",
     [
-        (Region(start=16, end=67), 0.62745098039215684),
-        (Region(start=302, end=407), 0.7047619047619048),
-        (Region(start=510, end=576), 0.8181818181818182),
+        (Region(start=16, end=67), ((13+19)/51)),
+        (Region(start=302, end=407), ((31+43)/105)),
+        (Region(start=510, end=576), ((26+28)/66)),
     ]
 )
 def test_gc_content(
@@ -20,8 +20,9 @@ def test_gc_content(
     expected: float,
 ):
     uorf = UORFCoordinates(five_utr=hbb_five_utr, uorf=region)
-
-    assert gc_content(five_sequence=hbb_five_utr_sequence, uorf=uorf) == expected
+    result = gc_content(five_sequence=hbb_five_utr_sequence, uorf=uorf)
+    
+    assert result == pytest.approx(expected, rel=1e-6)
 
 
 def test_uorf_ends_out_of_five_prime(
@@ -39,11 +40,11 @@ def test_uorf_ends_out_of_five_prime(
 @pytest.mark.parametrize(
         "region, bases, expected",
         [
-            ((Region(start=16, end=67)), 10, 0.9),
-            ((Region(start=302, end=407)), 10, 0.9),
-            ((Region(start=510, end=576)), 10, 0.6),
-            ((Region(start=510, end=576)), 700, 0.7291666666666666), # Number of bases out of 5'UTR region,
-            ((Region(start=510, end=576)), 8500, 0.7291666666666666), # so the GC content remains the same, clipped to 48 bases
+            ((Region(start=16, end=67)), 10, ((4+5)/10)),
+            ((Region(start=302, end=407)), 10, ((3+6)/10)),
+            ((Region(start=510, end=576)), 10, ((4+3)/10)),
+            ((Region(start=510, end=576)), 700, ((18+15)/47)), # Number of bases out of 5'UTR region,
+            ((Region(start=510, end=576)), 8500, ((18+15)/47)), # so the GC content remains the same, clipped to 48 bases
         ]
 )
 def test_gc_content_n_bases_downstream(
@@ -54,8 +55,9 @@ def test_gc_content_n_bases_downstream(
     expected: float, 
 ):
     uorf = UORFCoordinates(five_utr=hbb_five_utr, uorf=region)
+    result = gc_content_n_bases_downstream(five_sequence=hbb_five_utr_sequence, uorf=uorf, bases=bases) 
 
-    assert gc_content_n_bases_downstream(five_sequence=hbb_five_utr_sequence, uorf=uorf, bases=bases) == expected
+    assert result == pytest.approx(expected, rel=1e-6)
 
 
 @pytest.mark.parametrize(
@@ -64,8 +66,8 @@ def test_gc_content_n_bases_downstream(
             ((Region(start=16, end=67)), 10, 61),
             ((Region(start=302, end=407)), 10, 115),
             ((Region(start=510, end=576)), 10, 76),
-            ((Region(start=510, end=576)), 700, 114), 
-            ((Region(start=510, end=576)), 8500, 114),
+            ((Region(start=510, end=576)), 700, 113), 
+            ((Region(start=510, end=576)), 8500, 113),
         ]
 )
 def test_uorfs_plus_nts_downstream(
@@ -83,9 +85,9 @@ def test_uorfs_plus_nts_downstream(
 @pytest.mark.parametrize(
         "region, expected",
         [
-            ((Region(start=16, end=67)), 557),
-            ((Region(start=302, end=407)), 217),
-            ((Region(start=510, end=576)), 48),
+            ((Region(start=16, end=67)), 556),
+            ((Region(start=302, end=407)), 216),
+            ((Region(start=510, end=576)), 47),
         ]
 )
 def test_intercistonic_distances(
