c462ff6d3becee59d865930ed19d541308eef062
Alejandro Pinto Alba <alejandropintoalba@gmail.com>
2025-04-23 15:06:10 +0200

    Add class to determine and work with the AA produced by the variant

--- src/utrfx/variant_util.py
+++ src/utrfx/variant_util.py
@@ -323,3 +323,51 @@ class VariantClassifier:
             if uorf_region.start <= self._variant_cdna_pos <= uorf_region.end:
                 return uorf_index + 1
         return None
+    
+class VariantAA:
+
+    def __init__(
+        self,
+        five_prime_seq: str,
+        uorf_coordinates: Region,
+        variant_cdna_pos: int
+    ):
+        self._five_prime_seq = five_prime_seq
+        self._uorf_coordinates = uorf_coordinates
+        self._variant_cdna_pos = variant_cdna_pos
+        self._aa_dict = self._amino_acids_dictionary()
+
+    def _amino_acids_dictionary(self) -> dict:
+        """
+        Dictionary with each amino acid and its corresponding codon/s.
+        """
+        aa_dict = { 
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+        } 
+
+        return aa_dict
+    
+    def variant_codon(self) -> typing.Optional[str]:
+        """
+        Return the codon that contains the variant.
+        """
+        for i in range(self._uorf_coordinates.start, self._uorf_coordinates.end, 3):
+            codon = self._five_prime_seq[i:i + 3]
+            if i <= self._variant_cdna_pos < i + 3:
+                return codon
+        return None
